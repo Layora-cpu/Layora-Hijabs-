569f522925ee3c21b341f3aa7992c9d577fe98bc
Layora-cpu <layorahijabs@gmail.com>
2026-07-03 18:54:26 +0530

    Update and rename product.js to products.js

---
+++ js/products.js
@@ -0,0 +1,417 @@
+const products = [
+
+{
+id:"LAY001",
+name:"Moja Hand Sleeves",
+category:"Accessories",
+mrp:199,
+price:169,
+image:"images/products/lay001.jpg",
+badge:"BEST SELLER"
+},
+
+{
+id:"LAY002",
+name:"Zera Tube Cap",
+category:"Accessories",
+mrp:179,
+price:149,
+image:"images/products/lay002.jpg",
+badge:""
+},
+
+{
+id:"LAY003",
+name:"Zera Bonnet Cap",
+category:"Accessories",
+mrp:189,
+price:149,
+image:"images/products/lay003.jpg",
+badge:""
+},
+
+{
+id:"LAY004",
+name:"Moja Shoulder Sleeves",
+category:"Accessories",
+mrp:399,
+price:320,
+image:"images/products/lay004.jpg",
+badge:""
+},
+
+{
+id:"LAY005",
+name:"Zera Shoulder Sleeves",
+category:"Accessories",
+mrp:399,
+price:319,
+image:"images/products/lay005.jpg",
+badge:""
+},
+
+{
+id:"LAY006",
+name:"Scrunchies",
+category:"Accessories",
+mrp:259,
+price:149,
+image:"images/products/lay006.jpg",
+badge:""
+},
+
+{
+id:"LAY007",
+name:"Moja Loop Pin",
+category:"Accessories",
+mrp:299,
+price:199,
+image:"images/products/lay007.jpg",
+badge:""
+},
+
+{
+id:"LAY008",
+name:"Zera Shaper",
+category:"Accessories",
+mrp:199,
+price:119,
+image:"images/products/lay008.jpg",
+badge:""
+},
+
+{
+id:"LAY009",
+name:"Zera Tape",
+category:"Accessories",
+mrp:199,
+price:99,
+image:"images/products/lay009.jpg",
+badge:""
+},
+
+{
+id:"LAY010",
+name:"Plastic Pin",
+category:"Accessories",
+mrp:229,
+price:119,
+image:"images/products/lay010.jpg",
+badge:""
+},
+
+{
+id:"LAY011",
+name:"Moja Magnetic Pin",
+category:"Accessories",
+mrp:169,
+price:169,
+image:"images/products/lay011.jpg",
+badge:"NEW"
+},
+
+{
+id:"LAY012",
+name:"Ball Pin",
+category:"Accessories",
+mrp:99,
+price:70,
+image:"images/products/lay012.jpg",
+badge:""
+},
+
+{
+id:"LAY013",
+name:"Scarf Pin",
+category:"Accessories",
+mrp:125,
+price:99,
+image:"images/products/lay013.jpg",
+badge:""
+},
+
+{
+id:"LAY014",
+name:"Zera Luxe Jersey",
+category:"Jersey",
+mrp:349,
+price:319,
+image:"images/products/lay014.jpg",
+badge:"PREMIUM"
+},
+
+{
+id:"LAY015",
+name:"Moja Malesian Georgette",
+category:"Georgette",
+mrp:349,
+price:299,
+image:"images/products/lay015.jpg",
+badge:""
+}
+
+];
+{
+id:"LAY016",
+name:"Zera Malesian Georgette",
+category:"Georgette",
+mrp:319,
+price:299,
+image:"images/products/lay016.jpg",
+badge:""
+},
+
+{
+id:"LAY017",
+name:"Moja Jersey",
+category:"Jersey",
+mrp:399,
+price:299,
+image:"images/products/lay017.jpg",
+badge:"BEST SELLER"
+},
+
+{
+id:"LAY018",
+name:"Zera Water Colour Modal Cotton",
+category:"Modal",
+mrp:469,
+price:369,
+image:"images/products/lay018.jpg",
+badge:"PREMIUM"
+},
+
+{
+id:"LAY019",
+name:"Moja Organza",
+category:"Organza",
+mrp:299,
+price:299,
+image:"images/products/lay019.jpg",
+badge:""
+},
+
+{
+id:"LAY020",
+name:"Zera Crinkled Cotton",
+category:"Cotton",
+mrp:299,
+price:269,
+image:"images/products/lay020.jpg",
+badge:""
+},
+
+{
+id:"LAY021",
+name:"Cotton Print with Tassels",
+category:"Cotton",
+mrp:399,
+price:320,
+image:"images/products/lay021.jpg",
+badge:"NEW"
+},
+
+{
+id:"LAY022",
+name:"Organza Shimmer Rhinestone",
+category:"Organza",
+mrp:369,
+price:319,
+image:"images/products/lay022.jpg",
+badge:"PREMIUM"
+},
+
+{
+id:"LAY023",
+name:"Magnetic Hijabs",
+category:"Premium",
+mrp:469,
+price:369,
+image:"images/products/lay023.jpg",
+badge:"NEW"
+},
+
+{
+id:"LAY024",
+name:"Malaysian Cotton",
+category:"Cotton",
+mrp:449,
+price:269,
+image:"images/products/lay024.jpg",
+badge:""
+},
+
+{
+id:"LAY025",
+name:"Bubble Satin Stone",
+category:"Satin",
+mrp:399,
+price:319,
+image:"images/products/lay025.jpg",
+badge:""
+},
+
+{
+id:"LAY026",
+name:"Zera Luxe Satin",
+category:"Satin",
+mrp:349,
+price:299,
+image:"images/products/lay026.jpg",
+badge:"BEST SELLER"
+},
+
+{
+id:"LAY027",
+name:"Textured Satin",
+category:"Satin",
+mrp:399,
+price:319,
+image:"images/products/lay027.jpg",
+badge:""
+},
+
+{
+id:"LAY028",
+name:"Zera MG Triple Black",
+category:"Premium",
+mrp:469,
+price:349,
+image:"images/products/lay028.jpg",
+badge:"PREMIUM"
+},
+
+{
+id:"LAY029",
+name:"Ombre Chiffon",
+category:"Chiffon",
+mrp:299,
+price:269,
+image:"images/products/lay029.jpg",
+badge:""
+},
+
+{
+id:"LAY030",
+name:"Moja Strips Chiffon",
+category:"Chiffon",
+mrp:399,
+price:349,
+image:"images/products/lay030.jpg",
+badge:""
+},
+{
+id:"LAY031",
+name:"Rafka Heavy Chiffon",
+category:"Chiffon",
+mrp:449,
+price:369,
+image:"images/products/lay031.jpg",
+badge:""
+},
+
+{
+id:"LAY032",
+name:"Yusra Georgette",
+category:"Georgette",
+mrp:399,
+price:349,
+image:"images/products/lay032.jpg",
+badge:""
+},
+
+{
+id:"LAY033",
+name:"Yusra Chiffon",
+category:"Chiffon",
+mrp:399,
+price:349,
+image:"images/products/lay033.jpg",
+badge:""
+},
+
+{
+id:"LAY034",
+name:"Rafka Organza",
+category:"Organza",
+mrp:469,
+price:399,
+image:"images/products/lay034.jpg",
+badge:"PREMIUM"
+},
+
+{
+id:"LAY035",
+name:"Muna Satin",
+category:"Satin",
+mrp:469,
+price:399,
+image:"images/products/lay035.jpg",
+badge:""
+},
+
+{
+id:"LAY036",
+name:"Ombre Turkish Cotton",
+category:"Cotton",
+mrp:449,
+price:369,
+image:"images/products/lay036.jpg",
+badge:""
+},
+
+{
+id:"LAY037",
+name:"Soft Cotton Print",
+category:"Cotton",
+mrp:399,
+price:329,
+image:"images/products/lay037.jpg",
+badge:""
+},
+
+{
+id:"LAY038",
+name:"Shimmer Crush Satin",
+category:"Satin",
+mrp:499,
+price:419,
+image:"images/products/lay038.jpg",
+badge:"NEW"
+},
+
+{
+id:"LAY039",
+name:"Kids Jersey",
+category:"Kids",
+mrp:299,
+price:249,
+image:"images/products/lay039.jpg",
+badge:""
+},
+
+{
+id:"LAY040",
+name:"Kids Organza",
+category:"Kids",
+mrp:349,
+price:299,
+image:"images/products/lay040.jpg",
+badge:""
+},
+
+{
+id:"LAY041",
+name:"Tie Cap",
+category:"Accessories",
+mrp:199,
+price:149,
+image:"images/products/lay041.jpg",
+badge:""
+},
+
+{
+id:"LAY042",
+name:"Neck Cover",
+category:"Accessories",
+mr
